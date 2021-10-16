629ee455fdc1d711924f8e8c433dace472ebf83f
queil <queil@users.noreply.github.com>
2021-10-16 13:38:51 +0100

    Example: added the first one

---
+++ examples/Queil.FSharp.FscHost.Examples.Console/Program.fs
@@ -0,0 +1,26 @@
+﻿open Queil.FSharp.FscHost
+
+try
+
+  // compile a script and retrieve two properties out of it
+  let getScriptProperties () =
+    File "script.fsx"
+    |> CompilerHost.getScriptProperties2 Options.Default
+         (Property<string -> string>.Path "Script.export")
+         (Property<int list>.Path "Script.myPrimes")
+
+  let (helloWorld, primesOfTheDay) = getScriptProperties () |> Async.RunSynchronously
+
+  let myName = "Console Example"
+
+  myName |> helloWorld |> printfn "%s"
+
+  printfn "Primes of the day: "
+  primesOfTheDay |> Seq.iter (printfn "%i")
+
+with
+// you can handle more exceptions from the CompilerHost here
+| ScriptsPropertyNotFound(propertyName, foundProperties) ->
+  printfn "Couldn't find property: '%s'" propertyName
+  printfn "Found properties: "
+  foundProperties |> Seq.iter(printfn "%s")
---
+++ examples/Queil.FSharp.FscHost.Examples.Console/script.fsx
@@ -0,0 +1,5 @@
+let helloFromScript name = sprintf "HELLO FROM THE SCRIPT, %s" name
+
+let myPrimes = [2; 3; 5]
+
+let export = helloFromScript

d41a7aa095f97f91cebfd4519309c8275d01f1a8
queil <queil@users.noreply.github.com>
2022-07-31 16:36:12 +0100

    Some nonsense implementation that at least builds (STEP 2)

--- src/Queil.FSharp.FscHost/Plugin.fs
+++ src/Queil.FSharp.FscHost/Plugin.fs
@@ -4,22 +4,59 @@ module CE =
 
   type State<'a> =
     {
-      filePath: string
+      filePath: string option
+      plugin: 'a option
     }
+    with
+      static member internal Default: State<'a> = {
+        filePath = None
+        plugin = None
+      }
 
-  type PluginBuilder<'a>() =
+  and PluginBuilder<'a>() =
+     
      [<CustomOperation("file")>]
-     member x.File(path:string, state: State<'a>) =
-       {state with filePath = path}
-
+     member x.File(state: State<'a>, path: string) =
+       {state with filePath = Some path}
+     
+     member x.Delay(f: unit -> State<'a>) = f ()
+     
+     member x.Combine(builder: State<'a>, newState: State<'a>) =
+       { builder with
+            filePath = newState.filePath
+            plugin = newState.plugin
+       }
+     
+     member x.Yield (plugin: 'a) : State<'a> =
+       {State<'a>.Default with plugin = Some plugin}
+  
+     //member x.Return (plugin: 'a) =  {State<'a>.Default with plugin = Some plugin}
+     
+     member x.For(seq2:seq<'a>, func: 'a -> State<'a>) =
+       seq {
+         for x in seq2 do
+           yield func x 
+       }
+       
+     member x.Run(state: State<'a>) =
+       state.plugin
+  
+     member x.Zero() = State.Default
+  
   let plugin<'a> = PluginBuilder<'a>()
   
+open CE
+
 module Test =
   
   open CE
-  let plugin =
-    
-    
+  let plugin2 () =
+
     plugin<int> {
-        file "test"
+        
+        yield 3
+        yield 6
+        
     }
+    
+  

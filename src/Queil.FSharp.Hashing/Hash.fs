cd9a3e1fb39f8a6ab3572de31db667dbf2da8dbf
queil <4584075+queil@users.noreply.github.com>
2025-07-29 12:04:58 +0200

    Fixes (#38)
    * chore: fix hash
    
    * fix: make hashing thread-safe

--- src/Queil.FSharp.Hashing/Hash.fs
+++ src/Queil.FSharp.Hashing/Hash.fs
@@ -4,13 +4,18 @@ open System
 open System.IO
 open System.Text
 open System.Security.Cryptography
+open System.Threading
 
 [<RequireQualifiedAccess>]
 module Hash =
-    let private sha256Hasher = SHA256.Create()
+    let private sha256Hasher = new ThreadLocal<SHA256>(fun () -> SHA256.Create())
 
     let sha256 (s: string) =
-        s |> Encoding.UTF8.GetBytes |> sha256Hasher.ComputeHash |> Convert.ToHexString
+
+        s
+        |> Encoding.UTF8.GetBytes
+        |> sha256Hasher.Value.ComputeHash
+        |> Convert.ToHexString
 
     let short (s: string) = s[0..10].ToLowerInvariant()
 
